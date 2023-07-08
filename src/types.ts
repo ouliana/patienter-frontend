43a7f806828195c3c41ee524e009a4793bb7d469
ouliana <ouliana.kotik@gmail.com>
2023-07-08 14:21:30 +0300

    defined type Entry

--- src/types.ts
+++ src/types.ts
@@ -10,6 +10,43 @@ export enum Gender {
   Other = "other"
 }
 
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: string,
+    criteria: string
+  }
+}
+
+export type Entry =
+  HealthCheckEntry |
+  OccupationalHealthcareEntry |
+  HospitalEntry;
 export interface Patient {
   id: string;
   name: string;
@@ -20,3 +57,4 @@ export interface Patient {
 }
 
 export type PatientFormValues = Omit<Patient, "id" | "entries">;
+
